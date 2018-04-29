1253a359802fd9a4406397429cf050473963461f
askrepps <ask95208@gmail.com>
2018-04-29 19:00:31 -0400

    Added class stubs for host-based solver and GPU-based genetic solver

---
+++ GeneticSolver.cpp
@@ -0,0 +1,22 @@
+//////////////////////////////
+// GeneticSolver.cpp        //
+// Andrew Krepps            //
+// EN.605.417 Final Project //
+// 15 May 2018              //
+//////////////////////////////
+
+#include "GeneticSolver.h"
+
+#include "Path.h"
+#include "Puzzle.h"
+
+namespace gws
+{
+	GeneticSolver::~GeneticSolver() {}
+	
+	bool GeneticSolver::solvePuzzle(const Puzzle& puzzle, Path& path)
+	{
+		// TODO: find solution
+		return false;
+	}
+}
---
+++ HostSolver.cpp
@@ -0,0 +1,22 @@
+//////////////////////////////
+// HostSolver.cpp           //
+// Andrew Krepps            //
+// EN.605.417 Final Project //
+// 15 May 2018              //
+//////////////////////////////
+
+#include "HostSolver.h"
+
+#include "Path.h"
+#include "Puzzle.h"
+
+namespace gws
+{
+	HostSolver::~HostSolver() {}
+	
+	bool HostSolver::solvePuzzle(const Puzzle& puzzle, Path& path)
+	{
+		// TODO: find solution
+		return false;
+	}
+}
