33d56b4aecaaff2a7e8b4753daf0cceec4b85f2d
askrepps <ask95208@gmail.com>
2018-05-13 15:51:33 -0400

    Added CPU/GPU timing comparison and CPU puzzle size limit to main

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@
 // 15 May 2018              //
 //////////////////////////////
 
+#include "GeneticSolver.h"
 #include "HostSolver.h"
 #include "Path.h"
 #include "Puzzle.h"
@@ -14,6 +15,25 @@
 #include <chrono>
 #include <iostream>
 #include <string>
+
+void runSolver(gws::Solver* solver, const std::string& name, const gws::Puzzle& puzzle, gws::Path& path)
+{
+	auto start = std::chrono::high_resolution_clock::now();
+	bool solutionFound = solver->solvePuzzle(puzzle, path);
+	auto stop = std::chrono::high_resolution_clock::now();
+	float hostMs = std::chrono::duration<float>(stop - start).count()*1000.0f;
+	
+	if (solutionFound) {
+		std::cout << "Puzzle solved on " << name << std::endl;
+		std::cout << "Starting row: " << puzzle.getPointRow(path.getStartPointIndex()) << " | Starting col: " << puzzle.getPointCol(path.getStartPointIndex()) << std::endl;
+		std::cout << "Path: " << path << std::endl;
+	}
+	else {
+		std::cout << "No puzzle solution found on " << name << std::endl;
+	}
+	std::cout << name << " execution time: " << hostMs << " ms" << std::endl;
+	std::cout << std::endl;
+}
 
 int main(int argc, char** argv)
 {
@@ -26,6 +46,8 @@ int main(int argc, char** argv)
 	char* pointData;
 	char* edgeData;
 	char* spaceData;
+	
+	std::cout << "Reading puzzle from " << puzzleFile << std::endl << std::endl;
 	gws::Puzzle puzzle = gws::readPuzzle(puzzleFile, &pointData, &edgeData, &spaceData);
 	
 	// each point can only be visited once, so number of points can be used as the max path length
@@ -33,26 +55,23 @@ int main(int argc, char** argv)
 	char* moveData = new char[maxPathLength];
 	gws::Path path(moveData, maxPathLength);
 	
-	// solve puzzle (on host for now)
-	gws::Solver* solver = new gws::HostSolver();
-	
-	auto start = std::chrono::high_resolution_clock::now();
-	bool solutionFound = solver->solvePuzzle(puzzle, path);
-	auto stop = std::chrono::high_resolution_clock::now();
-	float ms = std::chrono::duration<float>(stop - start).count()*1000.0f;
-	
-	if (solutionFound) {
-		std::cout << "Puzzle solved" << std::endl;
-		std::cout << "Starting row: " << puzzle.getPointRow(path.getStartPointIndex()) << " | Starting col: " << puzzle.getPointCol(path.getStartPointIndex()) << std::endl;
-		std::cout << "Path: " << path << std::endl;
+	// solve puzzle and display timing metrics
+	if (puzzle.getWidth() < 7 && puzzle.getHeight() < 7) {
+		gws::Solver* hostSolver = new gws::HostSolver();
+		runSolver(hostSolver, "CPU", puzzle, path);
+		
+		delete hostSolver;
 	}
 	else {
-		std::cout << "No puzzle solution found" << std::endl;
+		std::cout << "Puzzle is too large to solve on host" << std::endl;
 	}
 	
-	std::cout << "Execution time: " << ms << " ms" << std::endl;
+	gws::Solver* gpuSolver = new gws::GeneticSolver(puzzle.getWidth(), puzzle.getHeight(), 1024, 10);
+	runSolver(gpuSolver, "GPU", puzzle, path);
 	
-	// clean up memory
+	delete gpuSolver;
+	
+	// clean up puzzle/path memory
 	delete [] pointData;
 	delete [] edgeData;
 	delete [] spaceData;
